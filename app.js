7d03430e490775d7f5fb663eb9bd302b77e9fa6d
Sean McMahon <sean.mcmahon87@gmail.com>
2021-03-02 13:12:32 +0000

    scroll effects added

--- app.js
+++ app.js
@@ -17,3 +17,23 @@
 //   splitScroll();
 // } });
 
+function scrollAppear(){
+  const project1Text = document.querySelector('.project1-text');
+  const project2Text = document.querySelector('.project2-text');
+  const project3Text = document.querySelector('.project3-text');
+  const project1Position = project1Text.getBoundingClientRect().top;
+  const project2Position = project2Text.getBoundingClientRect().top;
+  const project3Position = project3Text.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.3;
+  if(project1Position < screenPosition){
+    project1Text.classList.add('project-text-appear')
+  }
+  if(project2Position < screenPosition){
+    project2Text.classList.add('project-text-appear')
+  }
+  if(project3Position < screenPosition){
+    project3Text.classList.add('project-text-appear')
+  }
+}
+
+window.addEventListener('scroll', scrollAppear);
